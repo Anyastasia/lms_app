0b095da7f627df52b6ca253273fc29f5c1fb9faf
matt <41354651+mpbstrd@users.noreply.github.com>
2023-05-03 00:58:59 +0800

    languages displayed on a grid
    - icon ontap direct to screen to be fixed

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:lms_app/language_screens/cpp.dart';
 
+
+import 'language_screens/clang.dart';
+import 'language_screens/cpp.dart';
+import 'language_screens/css.dart';
+import 'language_screens/flutter.dart';
+import 'language_screens/html.dart';
+import 'language_screens/java.dart';
+import 'language_screens/javascript.dart';
+import 'language_screens/vue.dart';
 import 'language_screens/python.dart';
 
 class HomepageScreen extends StatefulWidget {
@@ -12,17 +22,48 @@ class HomepageScreen extends StatefulWidget {
 
 class _HomepageScreenState extends State<HomepageScreen> {
   final Map<String, String> _languages = {
-    'Python': 'assets/images/python.png',
-    'C': 'assets/images/clang.png',
-    'C++': 'assets/images/cpp.png',
-    'CSS': 'assets/images/css.png',
-    'JavaScript': 'assets/images/js.png',
-    'Java': 'assets/images/java.png',
-    'Vue': 'assets/images/vue.png',
-    'React': 'assets/images/react.png',
-    'Flutter': 'assets/images/flutter.png',
-    'HTML': 'assets/images/html.png',
-    };
+    'Python': 'images/python.png',
+    'C': 'images/clang.png',
+    'C++': 'images/cpp.png',
+    'CSS': 'images/css.png',
+    'Javascript': 'images/javascript.png',
+    'Java': 'images/java.png',
+    'Vue': 'images/vue.png',
+    'Flutter': 'images/flutter.png',
+    'HTML': 'images/html.png',
+  };
+
+
+  void _navigateToLanguageScreen(String language) {
+    if (language == "Python"){
+      Navigator.push(context,MaterialPageRoute(builder: (context) => const PythonScreen()),);
+    } else if (language == "C"){
+      Navigator.push(context,MaterialPageRoute(builder: (context) => const ClangScreen()),);
+    } else if (language == "C++"){
+      Navigator.push(context,MaterialPageRoute(builder: (context) => const CppScreen()),);
+    } else if (language == "CSS"){
+      Navigator.push(context,MaterialPageRoute(builder: (context) => const CssScreen()),);
+    } else if (language == "Javascript"){
+      Navigator.push(context,MaterialPageRoute(builder: (context) => const JavascriptScreen()),);
+    } else if (language == "Java"){
+      Navigator.push(context,MaterialPageRoute(builder: (context) => const JavaScreen()),);
+    } else if (language == "Vue"){
+      Navigator.push(context,MaterialPageRoute(builder: (context) => const VueScreen()),);
+    } else if (language == "Flutter"){
+      Navigator.push(context,MaterialPageRoute(builder: (context) => const FlutterScreen()),);
+    } else if (language == "HTML"){
+      Navigator.push(context,MaterialPageRoute(builder: (context) => const HtmlScreen()),);
+    }
+  }
+
+  void navScreen(String lang) {
+    if (lang == "Python"){
+      Navigator.push(context,MaterialPageRoute(builder: (context) => const PythonScreen()),);
+    } else if (lang == "C"){
+      Navigator.push(context,MaterialPageRoute(builder: (context) => const ClangScreen()),);
+    }
+  }
+
   @override
   Widget build(BuildContext context){
     return Scaffold(
@@ -40,6 +81,16 @@ class _HomepageScreenState extends State<HomepageScreen> {
                 ),
               ),
             ),
+            const Padding(
+              padding: EdgeInsets.only(left: 20.0),
+              child: Text(
+                'Courses',
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
             Expanded(
               child: GridView.count(
                 crossAxisCount: 3,
@@ -47,30 +98,27 @@ class _HomepageScreenState extends State<HomepageScreen> {
                 padding: const EdgeInsets.all(16.0),
                 mainAxisSpacing: 16.0,
                 crossAxisSpacing: 16.0,
-                children: List.generate(_languages.length, // index length
-                  (index) => Container(
+                children: List.generate(_languages.length, (index) {
+                  final language = _languages[index];
+                  return Container(
                     decoration: BoxDecoration(
                       color: Colors.grey[200],
                       borderRadius: BorderRadius.circular(8.0),
                     ),
                     child: InkWell(
                       onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => const PythonScreen()),
-                        );
+                        Navigator.push(context,MaterialPageRoute(builder: (context) => const PythonScreen()),);
                       },
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          const Icon(
-                            Icons.ac_unit,
-                            size: 70.0,
-                            color: Colors.blueGrey,
+                          Image.asset(
+                            _languages[_languages.keys.elementAt(index)]!,
+                            width: 50.0,
                           ),
                           const SizedBox(height: 8.0),
                           Text(
-                            "Item $index",
+                            _languages.keys.elementAt(index),
                             style: const TextStyle(
                               fontSize: 16.0,
                               fontWeight: FontWeight.bold,
@@ -79,8 +127,9 @@ class _HomepageScreenState extends State<HomepageScreen> {
                         ],
                       ),
                     ),
-                  ),
-                ),
+                  );
+                }),
+
               ),
             )
           ],
