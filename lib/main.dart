8a87a70365d1fa8fe9b68ab4e2928c85916f2b10
J-Allyson30 <jeanallyson.junsay@tup.edu.ph>
2023-05-03 13:50:37 +0800

    update padding only

--- lib/main.dart
+++ lib/main.dart
@@ -14,12 +14,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-        scaffoldBackgroundColor: Color.fromARGB(255, 108,99,255),
-        textTheme: const TextTheme(
-          bodyMedium: TextStyle(fontSize: 15.0, fontFamily: 'Poppins', color: Colors.white70, letterSpacing: 1),
-        )
-      ),
+          primarySwatch: Colors.blue,
+          scaffoldBackgroundColor: Color.fromARGB(255, 108, 99, 255),
+          textTheme: const TextTheme(
+            bodyMedium: TextStyle(
+                fontSize: 15.0,
+                fontFamily: 'Poppins',
+                color: Colors.white70,
+                letterSpacing: 1),
+          )),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
       debugShowCheckedModeBanner: false,
     );
@@ -38,12 +41,12 @@ class _MyHomePageState extends State<MyHomePage> {
   bool _visible = true;
   int counter = 0;
 
-  void nextBtn(){
-    setState((){
+  void nextBtn() {
+    setState(() {
       counter++;
-      if(counter == 1){
+      if (counter == 1) {
         _visible = false;
-      }else if(counter == 2){
+      } else if (counter == 2) {
         _visible = true;
         counter = 0;
         Navigator.push(
@@ -60,63 +63,56 @@ class _MyHomePageState extends State<MyHomePage> {
       title: 'My App',
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Color.fromARGB(255, 108,99,255),
-        body: Center(
-          child: Column(
-           children: [
-              Stack( 
-                children: [
-                  Visibility(
-                    visible: _visible,
-                    child: AnimatedOpacity(
-                      opacity: _visible ? 1.0 : 0.0,
-                      duration: const Duration(milliseconds: 200),
-                      child: Image(image: AssetImage("images/etivacicon.png")),
-                      )),
-                    Visibility(
-                    visible: !_visible,
-                    child: AnimatedOpacity(
-                    opacity: _visible ? 0.0 : 1.0,
+          backgroundColor: Color.fromARGB(255, 108, 99, 255),
+          body: Center(
+              child: Column(children: [
+            Stack(children: [
+              Visibility(
+                  visible: _visible,
+                  child: AnimatedOpacity(
+                    opacity: _visible ? 1.0 : 0.0,
                     duration: const Duration(milliseconds: 200),
-                    child: GestureDetector(
-                        child: Image(image: AssetImage("images/secondicon.png")),
-                      )
-                    )
-                  ),
-                ]
-              )
-            ]
-          )
-        ),
-        floatingActionButton: Padding(
-          padding: const EdgeInsets.only(bottom: 50, right: 20),
-          child:  FloatingActionButton.extended(
-            onPressed: ()=>{
-              nextBtn()
-            },
-            backgroundColor: Colors.white,
-            shape: const RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(Radius.circular(8.0))
+                    child: Padding(
+                      padding: EdgeInsets.only(top: 60.0),
+                      child: Image(image: AssetImage("images/etivacicon.png")),
+                    ),
+                    // child: Image(image: AssetImage("images/etivacicon.png")),
+                  )),
+              Visibility(
+                  visible: !_visible,
+                  child: AnimatedOpacity(
+                      opacity: _visible ? 0.0 : 1.0,
+                      duration: const Duration(milliseconds: 200),
+                      child: GestureDetector(
+                          child: Padding(
+                        padding: EdgeInsets.only(top: 60.0),
+                        child:
+                            Image(image: AssetImage("images/secondicon.png")),
+                      )))),
+            ])
+          ])),
+          floatingActionButton: Padding(
+            padding: const EdgeInsets.only(bottom: 50, right: 20),
+            child: FloatingActionButton.extended(
+              onPressed: () => {nextBtn()},
+              backgroundColor: Colors.white,
+              shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(8.0))),
+              label: Padding(
+                  padding: EdgeInsets.zero,
+                  child: Container(
+                      width: 80.0,
+                      // height: 20.0,
+                      alignment: Alignment.center,
+                      child: const Text(
+                        "Next",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                            color: Colors.black87),
+                      ))),
             ),
-            label: Padding(
-              padding: EdgeInsets.zero,
-              child: Container(
-                width: 80.0,
-                // height: 20.0,
-                alignment: Alignment.center,
-                child: const Text(
-                  "Next",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
-                    color: Colors.black87
-                  ),
-                )
-              )
-            ),
-          ),
-        )
-      ),
+          )),
     );
   }
 }
