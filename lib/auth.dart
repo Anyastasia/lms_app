5b32d541a3c26858e01d922a2ef3ee8d795b9d96
matt <41354651+mpbstrd@users.noreply.github.com>
2023-05-04 01:59:51 +0800

    firebase authentication working
    - needs to modify page structures / main.dart

--- lib/auth.dart
+++ lib/auth.dart
@@ -1,13 +1,33 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
 class Auth {
-  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
-  Future<void> registerWithEmailAndPassword(String email, String password) async {
-    final user = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+  User? get currentUser => _firebaseAuth.currentUser;
+
+  Stream<User?> get authStateChanges => _firebaseAuth.authStateChanges();
+
+  Future<void> signInWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
+    await _firebaseAuth.signInWithEmailAndPassword(
+      email: email, 
+      password: password
+    );
   }
 
-  Future <void> signInWithEmailAndPassword(String email, String password) async {
-    final user = await _auth.signInWithEmailAndPassword(email: email, password: password);
+  Future<void> createUserWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
+    await _firebaseAuth.createUserWithEmailAndPassword(
+      email: email, 
+      password: password
+    );
+  }
+
+  Future<void> signOut() async {
+    await _firebaseAuth.signOut();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,60 @@
 import 'package:flutter/material.dart';
-
-
+import 'package:firebase_core/firebase_core.dart';
+import 'widget_tree.dart';
+import 'firebase_options.dart';
 import 'navbar.dart';
-import 'firebase_test.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    debugPrint("3");
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const WidgetTree()
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({super.key, required this.title});
+//   final String title;
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
 
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'My App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('My App'),
-        ),
-        body: Center(
-          child: GestureDetector(
-            child: const Text('Go to my new screen!'),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const AuthScreen()),
-              );
-            },
-          ),
-        ),
-      ),
-    );
-  }
-}
+// class _MyHomePageState extends State<MyHomePage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'My App',
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: const Text('My App'),
+//         ),
+//         body: Center(
+//           child: GestureDetector(
+//             child: const Text('Go to my new screen!'),
+//             onTap: () {
+//               Navigator.push(
+//                 context,
+//                 MaterialPageRoute(builder: (context) => const Navigation()),
+//               );
+//             },
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
